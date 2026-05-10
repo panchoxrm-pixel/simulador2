e7630ef21e05fd5d40ddeb738f2d5e57de918df4
Francisco Rodríguez <panchoxrm@gmail.com>
2026-05-10 00:03:04 -0500

    Determinar las 2 secciones de la página, parámetros y clientes, y establecer la 1era parte de la lógica de funciones de clientes

--- simulador_completo.js
+++ simulador_completo.js
@@ -29,3 +29,81 @@ function guardarTasa(){
         mostrarTexto("mensajeTasa",`La tasa debe estar entre 10 % y 20 %`);
   }    
 }
+
+function guardarCliente(){
+  let valorCedula = recuperaraTexto("cedula");
+  let valorNombre = recuperaraTexto("nombre");
+  let valorApellido = recuperaraTexto("apellido");
+  let valorIngresos = recuperarInt("ingresos");
+  let valorEgresos = recuperarInt("egresos");
+
+  let clienteExistente = buscarCliente(valorCedula);
+
+  if(clienteExistente == null){
+  let clienteNuevo = {
+    cedula: valorCedula,
+    nombre: valorNombre,
+    apellido: valorApellido,
+    ingresos: valorIngresos,
+    egresos: valorEgresos
+  };
+  clientes.push(clienteNuevo);
+  } else {
+    clienteExistente.nombre = valorNombre;
+    clienteExistente.apellido = valorApellido;
+    clienteExistente.ingresos = valorIngresos;
+    clienteExistente.egresos = valorEgresos;  }
+  pintarClientes();  
+  }
+ 
+function pintarClientes(){
+  let bodyTabla = document.getElementById("tablaClientes");
+  let contenidoTabla = "";
+  for(let i = 0; i < clientes.length; i ++){
+  let arregloClientes = clientes[i]
+  contenidoTabla += 
+    `<tr>
+          <td> ${arregloClientes.cedula} </td>
+          <td> ${arregloClientes.nombre} </td>
+          <td> ${arregloClientes.apellido} </td>
+          <td> ${arregloClientes.ingresos} </td>
+          <td> ${arregloClientes.egresos} </td>
+          <td>
+            <button onclick="seleccionarCliente('${arregloClientes.cedula}')">Actualizar</button>
+            <button>Eliminar</button>
+          </td>
+        </tr>`
+  }
+  bodyTabla.innerHTML = contenidoTabla;
+}
+
+function buscarCliente(cedula){
+    let elementoCliente;
+    let clienteEncontrado = null;
+    for (let i = 0; i < clientes.length; i ++){
+        elementoCliente = clientes [i];
+        if(elementoCliente.cedula == cedula){
+            clienteEncontrado = elementoCliente;
+            break 
+        }
+    }
+    return clienteEncontrado;
+}
+
+function seleccionarCliente(cedula){
+  let clienteSeleccionado = buscarCliente(cedula);
+
+        mostrarTextoEnCaja("cedula",clienteSeleccionado.cedula);
+        mostrarTextoEnCaja("nombre",clienteSeleccionado.nombre);
+        mostrarTextoEnCaja("apellido",clienteSeleccionado.apellido);
+        mostrarTextoEnCaja("ingresos",clienteSeleccionado.ingresos);
+        mostrarTextoEnCaja("egresos",clienteSeleccionado.egresos);
+  }
+
+  function limpiar(){
+        mostrarTextoEnCaja("cedula","");
+        mostrarTextoEnCaja("nombre","");
+        mostrarTextoEnCaja("apellido","");
+        mostrarTextoEnCaja("ingresos","");
+        mostrarTextoEnCaja("egresos","");
+        }
